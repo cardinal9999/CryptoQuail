de711ca10edff3dc5b14cd68550539f36d9dd111
cardinal9999 <86199598+cardinal9999@users.noreply.github.com>
2021-07-06 12:40:09 -0400

    Update hash.py

--- cryptoquail/hash.py
+++ cryptoquail/hash.py
@@ -2,10 +2,6 @@ from hashlib import md5, sha256 as sha256_, sha512 as sha512_, shake as shake_
 def crc32(input_):
     from zlib import crc32 as crc32_
     return hex(crc32_(input_))
-def saltgen(length):
-    "generates a random salt for the hash"
-    from secrets import token_urlsafe
-    return token_urlsafe(length)
 def shake(input_):
     return shake_.digest(input_)
 def sha256(string):
