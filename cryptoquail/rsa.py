9ff98db8a360f9d46d3efc14ae136b6926808432
cardinal9999 <86199598+cardinal9999@users.noreply.github.com>
2021-07-18 10:59:43 -0400

    Update rsa.py

--- cryptoquail/rsa.py
+++ cryptoquail/rsa.py
@@ -7,16 +7,13 @@ Factor a number:
 https://www.calculatorsoup.com/calculators/math/prime-factors.php
 """
     
-def encrypt(number, encryption_key, public_key):
-    f = public_key
-    e = encryption_key
-    c = number ** e % f
-    return c
-def decrypt(ciphertext, decryption_key, prime_numbers):
-    public_keys = prime_numbers
-    p = public_keys[0]
-    q = public_keys[1]
-    f = p-1 * q-1
-    d = decryption_key
-    m = (ciphertext ** d) % f 
-    return m
+def generate_d(p, q, e):
+    J = (p-1)*(q-1)
+    a = 0
+    while (e * a) % J != 1:
+        a += 1
+    return a
+def encrypt(m, n, e):
+    return (m ** e) % n
+def decrypt(c, d, n):
+    return (c**d) % n
