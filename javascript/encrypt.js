073be3d8c6f01f2704a16b9c40407aa66698b46d
cardinal9999 <86199598+cardinal9999@users.noreply.github.com>
2021-09-12 16:23:44 -0400

    Update encrypt.js

--- javascript/encrypt.js
+++ javascript/encrypt.js
@@ -1,8 +1,18 @@
+function charCodeAt(input){
+  var keys = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#%&()*^$@`~-_=][+{\ }|</>,.;:'\"" ;
+  var values = [];
+  for (var k = 0; k < keys.length; k++){
+    values.push(k);
+  }
+  for (var i = 0; i < keys.length; i++){
+    if (keys[i] === input){return values[i]}
+  }
+}
 function encode(string) {
     var number = "0x";
     var length = string.length;
     for (var i = 0; i < length; i++)
-        number += string.charCodeAt(i).toString(16);
+        number += charCodeAt(string[i]).toString(16);
     return number;
 }
 export function crypt(input, key) {
@@ -10,7 +20,7 @@ export function crypt(input, key) {
     var output = [];
     int1 = encode(key);
     for (var i = 0; i < input.length; i++) {
-        var charCode = input.charCodeAt(i) ^ (key[i % key.length] ^ int1).charCodeAt(0);
+        var charCode = charCodeAt(charCodeAt(input[i]) ^ (key[i % key.length] ^ int1)[0]);
         output.push(String.fromCharCode(charCode));
         }
     return output.join("");
