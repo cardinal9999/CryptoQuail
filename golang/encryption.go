defa6054a8e470dd1e99ec3925ac37e0ff694c4c
cardinal9999 <86199598+cardinal9999@users.noreply.github.com>
2021-09-22 21:15:20 -0400

    Update encryption.go

--- golang/encryption.go
+++ golang/encryption.go
@@ -1,8 +1,17 @@
 package cryptoquail
+// UNFINISHED!
+import (
+	"encoding/hex"
+	"fmt"
+	"math/big"
+)
 func crypt(input, key string) (output string) {
 	L := len(key)
+	u1 := hex.EncodeToString([]byte(key)) 
+	u := new(big.Int)
+	u.SetString(u1, 16)
 	for i := range input {
-		output += string(input[i] ^ key[i%L])
+		output += string(input[i] ^ key[i%L] ^ (u.Int64() % 100)) // Fix this: convert the bytes (key [i%l]) to integer
 	}
 	return output
 }
